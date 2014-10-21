f147e7dbe479a82aa41c2add2e0e59644abd3929
Andrew J Dyck <web@andrewdyck.com>
2014-10-20 20:08:30 -0600

    write function call

--- _bin/parse_api_data.r
+++ _bin/parse_api_data.r
@@ -8,8 +8,8 @@ clean_fao_data()
 
 prep_fao_fishstat_data(out_data)
 
-output_country_data(country_list)
-output_species_data(species_list)
+output_country_json(country_list)
+output_species_json(species_list)
 
 #writeLines(
 #  toJSON(data.frame(version=0.1, url='http://api.openfisheries.org')),
